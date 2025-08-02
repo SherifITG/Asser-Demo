991333028b1e7c67e1e12a8ef948849ebd814b51
sherif112 <sherif.salah.eldien112@gmail.com>
2025-08-02 03:07:31 +0300

    Initial commit

---
+++ src/main/java/com/asser/asser_demo/AsserDemoApplication.java
@@ -0,0 +1,13 @@
+package com.asser.asser_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsserDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsserDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asser/asser_demo/AsserDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asser.asser_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsserDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
